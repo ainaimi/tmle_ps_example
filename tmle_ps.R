67fd82870d2ed378c720671e480a48e9a3623550
ain <ashley.naimi@pitt.edu>
2019-02-11 22:56:38 -0500

    added section on obtaining outcome predictions from tmle object

--- tmle_ps.R
+++ tmle_ps.R
@@ -14,6 +14,19 @@ sl.lib <- c(ranger_learner$names,svm_learner$names,"SL.glm")
 
 tmle_obj <- tmle(Y, A, W, family="gaussian", Q.SL.library = sl.lib,g.SL.library = sl.lib)
 
-ps_data <- tibble(A=as.factor(A),PS=tmle_obj$g$g1W)
+## propensity score
 
+ps_data <- tibble(A=as.factor(A),PS=tmle_obj$g$g1W)
 ggplot(ps_data) + geom_density(aes(PS,group=A,color=A)) + ylab("Density") + xlab("Propensity Score")
+
+
+## outcome predictions
+# create dataset for merging
+tabl_data <- tibble(A=as.factor(A),W1=W[,1],W2=W[,2],W3=W[,3])
+# extract predictions for observed exposure
+tabl_data$predicted_outcomes <- ifelse(A==0,tmle_obj$Qstar[,1],tmle_obj$Qstar[,2])
+
+# extract median
+tabl_data %>% filter(abs(predicted_outcomes - median(predicted_outcomes)) == min(abs(predicted_outcomes - median(predicted_outcomes))))
+# extract min and max
+tabl_data %>% filter(predicted_outcomes %in% c(min(predicted_outcomes),max(predicted_outcomes)))
